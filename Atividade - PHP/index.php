6938b65cbf095ecb60494d6ca3f59ee5ba6a86d6
Erik Takeuti <116272608+ErikTakeuti@users.noreply.github.com>
2023-09-06 15:49:29 -0300

    Add files via upload

---
+++ Atividade - PHP/index.php
@@ -0,0 +1,156 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+    <link type="text/css" rel="stylesheet" href="css/materialize.min.css"  media="screen,projection"/>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+  </head>
+  <body>
+    <nav>
+      <div class="nav-wrapper blue lighten-3">
+        <a href="#" class="brand-logo center">SITE</a>
+        <ul class="right">
+          <li><a href="agenda.php">Agenda</a></li>
+          <li><a href="consulta.php">Consulta</a></li>
+          <li><a href="Localizacao.php">Localização</a></li>
+        </ul>
+      </div>
+    </nav>
+
+    <div class="container">
+    <div class="carousel carousel-slider">
+      <a class="carousel-item" href="#one!"><img src="img/01.png"></a>
+      <a class="carousel-item" href="#two!"><img src="img/02.png"></a>
+      <a class="carousel-item" href="#three!"><img src="img/03.png"></a>
+      <a class="carousel-item" href="#four!"><img src="img/unimad.png"></a>
+    </div>
+
+  <div class="row">
+      <div class="col s4">
+        <div class="card">
+          <div class="card-image">
+            <img src="img/carlos.png">
+            <span class="card-title blue write-text">Dr.Carlos</span>
+          </div>
+          <div class="card-content">
+            <p>Dentista principal, profissonal excelente, está na área atuando a mais de 9 anos.</p>
+          </div>
+          <div class="card-action">
+            <a href="#">Sobre o médico</a>
+          </div>
+        </div>
+      </div>
+
+      <div class="col s4">
+        <div class="card">
+          <div class="card-image">
+            <img src="img/andre.png">
+            <span class="card-title blue write-text">Dr.Andre</span>
+          </div>
+          <div class="card-content">
+            <p>I am a very simple card. I am good at containing small bits of information.
+            I am convenient because I require little markup to use effectively.</p>
+          </div>
+          <div class="card-action">
+            <a href="#">This is a link</a>
+          </div>
+        </div>
+      </div>
+
+      <div class="col s4">
+        <div class="card">
+          <div class="card-image">
+            <img src="img/jose.png">
+            <span class="card-title blue write-text">Dr.Jose</span>
+          </div>
+          <div class="card-content">
+            <p>I am a very simple card. I am good at containing small bits of information.
+            I am convenient because I require little markup to use effectively.</p>
+          </div>
+          <div class="card-action">
+            <a href="#">This is a link</a>
+          </div>
+        </div>
+      </div>
+
+      <div class="col s4">
+        <div class="card">
+          <div class="card-image">
+            <img src="img/maria.png">
+            <span class="card-title blue write-text">Dra.Maria</span>
+          </div>
+          <div class="card-content">
+            <p>I am a very simple card. I am good at containing small bits of information.
+            I am convenient because I require little markup to use effectively.</p>
+          </div>
+          <div class="card-action">
+            <a href="#">This is a link</a>
+          </div>
+        </div>
+      </div>
+
+      <div class="col s4">
+        <div class="card">
+          <div class="card-image">
+            <img src="img/raquel.png">
+            <span class="card-title blue write-text">Dra.Raquel</span>
+          </div>
+          <div class="card-content">
+            <p>I am a very simple card. I am good at containing small bits of information.
+            I am convenient because I require little markup to use effectively.</p>
+          </div>
+          <div class="card-action">
+            <a href="#">This is a link</a>
+          </div>
+        </div>
+      </div>
+
+      <div class="col s4">
+        <div class="card">
+          <div class="card-image">
+            <img src="img/ana.png">
+            <span class="card-title blue write-text">Dra.Ana</span>
+          </div>
+          <div class="card-content">
+            <p>I am a very simple card. I am good at containing small bits of information.
+            I am convenient because I require little markup to use effectively.</p>
+          </div>
+          <div class="card-action">
+            <a href="#">This is a link</a>
+          </div>
+        </div>
+      </div>
+  </div>
+  </div>
+
+  <footer class="page-footer blue">
+    <div class="container">
+      <div class="row">
+        <div class="col l6 s12">
+          <h5 class="white-text">SITE</h5>
+          <p class="grey-text text-lighten-4">Erik Takeuti & Antônio Carlos</p>
+        </div>
+        <div class="col l4 offset-l2 s12">
+          <h5 class="white-text">Links</h5>
+            <ul>  
+              <li><a class="grey-text text-lighten-3" href="#!">Link 1</a></li>
+              <li><a class="grey-text text-lighten-3" href="#!">Link 2</a></li>
+              <li><a class="grey-text text-lighten-3" href="#!">Link 3</a></li>
+              <li><a class="grey-text text-lighten-3" href="#!">Link 4</a></li>
+            </ul>
+        </div>
+      </div>
+    </div>
+        <div class="footer-copyright">
+          <div class="container">
+            © 2014 Copyright Erik & Carlos
+            <a class="grey-text text-lighten-4 right" href="#!">More Links</a>
+          </div>
+        </div>
+  </footer>
+            
+    <script type="text/javascript" src="https://code.jquery.com/jquery-3.2.1.min.js"></script>
+    <script type="text/javascript" src="js/materialize.min.js"></script>
+    <script>$('.carousel.carousel-slider').carousel({fullWidth: true});</script>
+  </body>
+</html>
